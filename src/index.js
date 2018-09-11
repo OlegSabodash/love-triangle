29cb4cc642f29d785cb9a495744afbe4ab3cdd40
unknown <o.sabodash@mail.ru>
2018-09-11 12:31:00 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+
+
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  preferences.unshift(-1);
+  var count = 0;
+  var one;
+  var two;
+  var three;
+  for (let i = 1; i < preferences.length; i++) {
+    if (preferences[i] == -1) continue;
+    one = preferences[i];
+    two = preferences[one];
+    three = preferences[two];
+
+    if(one == two || two == three) continue;
+    if (three == i) {
+      count = count + 1;
+      preferences[i] = -1;
+      preferences[one] = -1;
+      preferences[two] = -1;
+    }
+  }
+  return count;
 };
